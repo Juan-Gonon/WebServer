3d177322cbebb8d847aac3fc5f6aa9cf74971040
Juan Gonon <juanfegonon27@gmail.com>
2024-09-11 11:09:07 -0600

    res index.html

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import http from 'http'
+import fs from 'fs'
 
 const server = http.createServer((req, res) => {
   console.log(req.url)
@@ -9,10 +10,22 @@ const server = http.createServer((req, res) => {
   // res.write(`<h1>URL ${req.url}</h1>`)
   // res.end()
 
-  const data = { name: 'John Doe', age: 30, city: 'New York' }
+  // const data = { name: 'John Doe', age: 30, city: 'New York' }
 
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(data))
+  // res.writeHead(200, { 'Content-Type': 'application/json' })
+  // res.end(JSON.stringify(data))
+
+  if (req.url === '/') {
+    const htmlFile = fs.readFileSync('./public/index.html', 'utf-8')
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+
+    res.end(htmlFile)
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' })
+    res.end()
+  }
 })
 
 server.listen(8080, () => {
