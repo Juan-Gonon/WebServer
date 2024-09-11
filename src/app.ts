b31603c715bf96540d50a2af14849d633f59a81b
Juan Gonon <juanfegonon27@gmail.com>
2024-09-11 10:52:34 -0600

    res type application/json

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import http from 'http'
 const server = http.createServer((req, res) => {
   console.log(req.url)
 
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
+  // res.writeHead(200, {
+  //   'Content-Type': 'text/html'
+  // })
+  // res.write(`<h1>URL ${req.url}</h1>`)
+  // res.end()
 
-  res.write(`<h1>URL ${req.url}</h1>`)
+  const data = { name: 'John Doe', age: 30, city: 'New York' }
 
-  res.end()
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(data))
 })
 
 server.listen(8080, () => {
