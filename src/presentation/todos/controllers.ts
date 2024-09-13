7577920f9f57d8d46b72e3de0f14b1f316a026e1
Juan Gonon <juanfegonon27@gmail.com>
2024-09-13 11:13:16 -0600

    array todos y getTods Response

--- src/presentation/todos/controllers.ts
+++ src/presentation/todos/controllers.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express'
 
+const todos = [
+  { id: 1, text: 'Buy milk', createAt: new Date() },
+  { id: 2, text: 'Buy milk', createAt: null },
+  { id: 3, text: 'Buy milk', createAt: new Date() }
+]
+
 export class TodosController {
   // DI
   constructor () {}
 
-  public getTodos = (req: Request, res: Response): void => {
-    res.json([
-      { id: 1, text: 'Buy milk', createAt: new Date() },
-      { id: 2, text: 'Buy milk', createAt: null },
-      { id: 3, text: 'Buy milk', createAt: new Date() }
-    ])
+  public getTodos = (req: Request, res: Response): Response => {
+    return res.json(todos)
   }
 }
