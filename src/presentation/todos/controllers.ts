da1116c6f1313acd3c16f754bdeb9f2918928437
Juan Gonon <juanfegonon27@gmail.com>
2024-09-13 19:04:09 -0600

    update

--- src/presentation/todos/controllers.ts
+++ src/presentation/todos/controllers.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { Request, Response } from 'express'
 
 const todos = [
-  { id: 1, text: 'Buy milk', createAt: new Date() },
-  { id: 2, text: 'Buy milk', createAt: null },
-  { id: 3, text: 'Buy milk', createAt: new Date() }
+  { id: 1, text: 'Buy milk', completedAt: new Date() },
+  { id: 2, text: 'Buy milk', completedAt: null },
+  { id: 3, text: 'Buy milk', completedAt: new Date() }
 ]
 
 export class TodosController {
@@ -30,11 +31,31 @@ export class TodosController {
     const newTodo = {
       id: todos.length + 1,
       text,
-      createAt: null
+      completedAt: null
 
     }
 
     todos.push(newTodo)
     res.json(newTodo)
+  }
+
+  public updateTodo = (req: Request, res: Response): Response | undefined => {
+    const id = +req.params.id
+
+    if (isNaN(id)) return res.status(400).json({ error: 'ID argument is not a number' })
+
+    const todo = todos.find((todo) => todo.id === id)
+    if (!(todo !== undefined)) return res.status(404).json({ error: `Todo with id ${id} not found` })
+
+    const { text, completedAt } = req.body
+    // if (!text) res.status(400).json({ error: 'Text property is required' })
+
+    todo.text = text || todo.text;
+
+    (completedAt === 'null')
+      ? todo.completedAt = null
+      : todo.completedAt = new Date(completedAt || todo.completedAt)
+
+    res.json(todo)
   }
 }
