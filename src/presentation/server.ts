c5b45d1fcf80cbcb4d83fb074eaaf41ac61a9cf1
Juan Gonon <juanfegonon27@gmail.com>
2024-10-08 15:59:33 -0600

    compression middleware

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express'
 import path from 'path'
+import compression from 'compression'
 
 interface Options {
   port: number
@@ -25,6 +26,7 @@ export class Server {
     // Middlewares
     this.app.use(express.json()) // raw
     this.app.use(express.urlencoded({ extended: true })) // x-www-form-Urlencoded
+    this.app.use(compression())
 
     // public Folder
     this.app.use(express.static(this.publicPath))
