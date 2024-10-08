196f9df7989d7214875fffc0ef9f7d8762ceaef9
Juan Gonon <juanfegonon27@gmail.com>
2024-10-08 14:58:16 -0600

    archivo barril use-case

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,3 +2,5 @@ export * from './datasources/todo.datasource'
 export * from './DTOs'
 export * from './entities/todo.entity'
 export * from './repositories/todo.repository'
+
+export * from './use-cases'
