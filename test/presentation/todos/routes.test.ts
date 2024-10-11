986ee18287dd4991b5ce24f6380f0d2c85ec107b
Juan Gonon <juanfegonon27@gmail.com>
2024-10-11 13:14:03 -0600

    Prueba - GET - api/todos/id

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -9,10 +9,13 @@ describe('Todo route testing', () => {
     testServer.close()
   })
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany()
+  })
+
   const todo1 = { text: 'Hola Mundo 1' }
   const todo2 = { text: 'Hola Mundo 2' }
   it('should return TODOs api/todos', async () => {
-    await prisma.todo.deleteMany()
     await prisma.todo.createMany({
       data: [todo1, todo2]
     })
@@ -24,5 +27,19 @@ describe('Todo route testing', () => {
     expect(body.length).toBe(2)
     expect(body[0].text).toBe(todo1.text)
     expect(body[1].text).toBe(todo2.text)
+  })
+
+  it('should return a TODO api/todos/:id', async () => {
+    const todo = await prisma.todo.create({ data: todo1 })
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${todo.id}`)
+      .expect(200)
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo.text,
+      completedAt: todo.completedAt
+    })
   })
 })
